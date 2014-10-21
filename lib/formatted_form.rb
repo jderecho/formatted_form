1c3a1b38a0ce7c2b9f96f7549b1c367dd15747e3
Neil R. Zamora <neil.r.zamora@gmail.com>
2014-10-21 18:45:58 +0800

    added prepend_required_mark feature

--- lib/formatted_form.rb
+++ lib/formatted_form.rb
@@ -1,4 +1,5 @@
 require 'formatted_form/version'
 require 'formatted_form/engine'
+require 'formatted_form/config'
 require 'formatted_form/form_builder'
 require 'formatted_form/view_helper'
---
+++ lib/formatted_form/config.rb
@@ -0,0 +1,49 @@
+module FormattedForm
+  
+  def self.prepend_required_mark=(required_mark_value)
+    @prepend_required_mark = PrependRequiredMarkupConfig.new(required_mark_value)
+  end
+
+  def self.prepend_required_mark
+    @prepend_required_mark
+  end
+
+  protected
+
+  class PrependRequiredMarkupConfig
+
+    def initialize(required_mark_value)
+      @required_mark_value = setup(required_mark_value)
+    end
+
+    def been_setup?
+      @required_mark_value.present?
+    end
+
+    def [](name)
+      @required_mark_value[name]
+    end
+
+    private
+
+    def setup(required_mark_value)
+      if required_mark_value.nil?
+        false
+      elsif required_mark_value.is_a?(Hash)
+        defaults(
+            required_mark_value[:mark],
+            required_mark_value[:tag],
+            required_mark_value[:class]
+          )
+      elsif required_mark_value.is_a?(TrueClass)
+        defaults
+      else
+        defaults(required_mark_value.to_s)
+      end
+    end
+
+    def defaults(mark='*', tag=:span, css_class='control-label-required-mark')
+      {tag: :span, mark: mark, class: css_class}
+    end
+  end
+end
